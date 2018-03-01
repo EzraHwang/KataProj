f30a941b3df10f7deae199362b516efc2b572e69
EzraHwang <36685697+EzraHwang@users.noreply.github.com>
2018-03-01 23:04:04 +0800

    Kata - ROT13

---
+++ Rot13/Rot13/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rot13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rot13 : ");
+            String input = Console.ReadLine();
+            Console.WriteLine(Rot13(input));
+
+        }
+
+        public static String Rot13(string input)
+        {
+            String Ans = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Ans = Ans + Transfer(input[i]).ToString();
+                }
+
+            return Ans;
+
+
+        }
+
+        public static char Transfer(Char character)
+        {
+            int AscNum = 0;
+            Char output = 'A';
+
+            AscNum = Convert.ToInt32(character);
+
+            if (AscNum >= 65 && AscNum <= 90)
+            {
+                if (AscNum + 13 > 90)
+                {
+                    output = Convert.ToChar(AscNum - 13);
+                }
+                else
+                    output = Convert.ToChar(AscNum + 13);
+            }
+            else if (AscNum >= 97 && AscNum <= 122)
+            {
+                if (AscNum + 13 > 122)
+                {
+                    output = Convert.ToChar(AscNum - 13);
+                }
+                else
+                    output = Convert.ToChar(AscNum + 13);
+            }
+            else
+                output = Convert.ToChar(AscNum);
+
+            return output;
+        }
+
+
+    }
+}
