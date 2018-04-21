f106aa0a8c662685f9d59a004ad8b9e650af573c
EzraHwang <36685697+EzraHwang@users.noreply.github.com>
2018-04-21 11:03:00 +0800

    BandNameGenerator

---
+++ BandNameGenerator/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(bng("aabbcca"));
+            Console.ReadKey();
+        }
+
+        public static string bng(string str)
+        {
+            if (str.Last() == str.First())
+                return str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1) + str.Substring(1, str.Length - 1);
+            return "The " + str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1);
+    }
+}
