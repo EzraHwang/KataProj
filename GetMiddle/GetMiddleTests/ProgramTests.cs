f58233d109197d04ee713143b84ed527cf0457f3
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-07-26 19:01:58 +0800

    GetMiddle

---
+++ GetMiddle/GetMiddleTests/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetMiddle.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetMiddleTest()
+        {    
+            Assert.AreEqual("f", new TestProgram().GetMiddle("qwefqwe"));
+            Assert.AreEqual("fc", new TestProgram().GetMiddle("qwefcqwe"));
+            Assert.AreEqual("A", new TestProgram().GetMiddle("A"));
+        }
+    }
+}
