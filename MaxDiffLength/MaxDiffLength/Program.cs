a03111587f6e59f2e65bcb709312e580361f28b2
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-08-11 13:16:51 +0800

    MaxDiffLength

---
+++ MaxDiffLength/MaxDiffLength/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaxDiffLength
+{
+    public class MaxDiffLengthProgram
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        public static int MaxDiffLength(string[] inputA, string[] inputB)
+        {
+            var ans = -1;
+            for (var a = 0; a < inputA.Length; a++)
+            {
+                for (var b = 0; b < inputB.Length; b++)
+                {
+                    if (Math.Abs(inputA[a].Length - inputB[b].Length) > ans)
+                    {
+                        ans = Math.Abs(inputA[a].Length - inputB[b].Length);
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ MaxDiffLength/MaxDiffLengthTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaxDiffLength;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxDiffLength.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MaxDiffLengthTest()
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz" };
+            string[] s2 = new string[] { "cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww" };
+            Assert.AreEqual(13, MaxDiffLengthProgram.MaxDiffLength(s1, s2));
+        }
+    }
+}
