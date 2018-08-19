2e2582b5fa3730eef5e03dd5b4f113af7ff6fd63
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-08-19 21:06:59 +0800

    ExesAndOhs

---
+++ Exes and Ohs/Exes and Ohs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Exes_and_Ohs
+{
+    public class Exes_and_Ohs
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        public static bool ExesAndOhs(string input)
+        {
+            return input.ToLower().Count(o => o == 'o') == input.ToLower().Count(x => x == 'x') ? true : false;
+        }
+    }
+}
---
+++ Exes and Ohs/Exes and OhsTests/Exes_and_OhsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exes_and_Ohs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exes_and_Ohs.Tests
+{
+    [TestClass()]
+    public class Exes_and_OhsTests
+    {
+        [TestMethod()]
+        public void ExesAndOhsTest()
+        {
+            Assert.AreEqual(true, Exes_and_Ohs.ExesAndOhs("xxxooo"));
+            Assert.AreEqual(true, Exes_and_Ohs.ExesAndOhs("xxxXooOo"));
+            Assert.AreEqual(false, Exes_and_Ohs.ExesAndOhs("xxx23424esdsfvxXXOOooo"));
+            Assert.AreEqual(false, Exes_and_Ohs.ExesAndOhs("xXxxoewrcoOoo"));
+            Assert.AreEqual(false, Exes_and_Ohs.ExesAndOhs("XxxxooO"));
+            Assert.AreEqual(true, Exes_and_Ohs.ExesAndOhs("zssddd"));
+            Assert.AreEqual(false, Exes_and_Ohs.ExesAndOhs("Xxxxertr34"));
+        }
+    }
+}
