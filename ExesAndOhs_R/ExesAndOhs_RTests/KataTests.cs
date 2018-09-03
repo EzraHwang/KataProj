40516ab6a08ff2fadcf2f6e402641b73f880ae08
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-09-03 22:00:29 +0800

    Rewrite

---
+++ ExesAndOhs_R/ExesAndOhs_RTests/KataTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExesAndOhs_R;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs_R.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void OAndXIsEqual()
+        {
+            Assert.AreEqual(true, Kata.ExesAndOhs("xxxooo"));
+            Assert.AreEqual(true, Kata.ExesAndOhs("xxxXooOo"));
+        }
+        [TestMethod()]
+        public void OAndXIsNotEqual()
+        {
+            Assert.AreEqual(false, Kata.ExesAndOhs("xxx23424esdsfvxXXOOooo"));
+            Assert.AreEqual(false, Kata.ExesAndOhs("xXxxoewrcoOoo"));
+            Assert.AreEqual(false, Kata.ExesAndOhs("XxxxooO"));
+            Assert.AreEqual(false, Kata.ExesAndOhs("Xxxxertr34"));
+        }
+        [TestMethod()]
+        public void NoOAndX()
+        {
+            Assert.AreEqual(true, Kata.ExesAndOhs("zssddd"));
+        }
+    }
+}
