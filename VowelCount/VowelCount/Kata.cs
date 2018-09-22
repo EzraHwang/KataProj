83c4829bc8db3f05576a7e4627ad89eca41d840d
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-09-22 14:04:06 +0800

    Vowel Count

---
+++ VowelCount/VowelCount/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace VowelCount
+{
+    public class Kata
+    {
+        public int Vowel(string input)
+        {
+            //return input.Trim().Count(a => a == 'a') + input.Trim().Count(e => e == 'e') + input.Trim().Count(i => i == 'i') + input.Trim().Count(o => o == 'o') + input.Trim().Count(u => u == 'u');
+            //refactor
+            return input.Trim().Count(i => "aeiou".Contains(i));
+        }
+    }
+}
---
+++ VowelCount/VowelCountTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace VowelCount.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase("qzwsxdcrfv", 0,Description = "qzwsxdcrfv_Result_Shoild_Be_0")]
+        [TestCase("qwr  ty", 0, Description = "qwr  ty_Result_Shoild_Be_0")]
+        [TestCase("aaaeeelsdkjf", 6, Description = "aaaeeelsdkjf_Result_Shoild_Be_6")]
+        [TestCase("a e i o u", 5, Description = "a e i o u_Result_Shoild_Be_5")]
+        [TestCase("a", 1, Description = "a_Result_Shoild_Be_1")]
+        public void VowelTest(string input, int expected)
+        {
+            Assert.AreEqual(expected, new Kata().Vowel(input));
+        }
+    }
+}
