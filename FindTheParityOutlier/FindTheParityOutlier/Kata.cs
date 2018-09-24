e2efabb0b9e9d68472e7662f3327ae9237c26f5e
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-09-24 22:55:46 +0800

    Find the Parity outlier

---
+++ FindTheParityOutlier/FindTheParityOutlier/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheParityOutlier
+{
+    public class Kata
+    {
+        public int ParityOutlier(int[] input)
+        {
+            return input.Count(i => (Math.Abs(i) + 1) % 2 == 0) != 1
+                ? input.First(i => (Math.Abs(i) + 2) % 2 == 0)
+                : input.First(i => (Math.Abs(i) + 1) % 2 == 0);
+        }
+    }
+}
---
+++ FindTheParityOutlier/FindTheParityOutlierTests/KataTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace FindTheParityOutlier.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase(new[] { 2, 3, 5, 7, 9 }, 2, Description = "Even_In_Odds_return_Even")]
+        [TestCase(new[] { 12, -13, 25, 37, 49 }, 12, Description = "Even_In_Odds_With_Negative_return_Even")]
+        [TestCase(new[] { 1, 2, 4, 6, 8, 10 }, 1, Description = "Odd_In_Evens_return_Odd")]
+        [TestCase(new[] { 91, 92, 2234, -4116, 6788, 310 }, 91, Description = "Odd_In_Evens_With_Negative_return_Odd")]
+        public void ParityOutlierTest(int[] input, int expect)
+        {
+            Assert.AreEqual(expect,new Kata().ParityOutlier(input));
+        }
+    }
+}
