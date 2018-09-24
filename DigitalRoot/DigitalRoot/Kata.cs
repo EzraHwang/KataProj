fa20b05fa5965d1036b6b794960075b0d2c3079a
TITANSOFT\ezra.hwang23 <ezra.hwang@titansoft.com.sg>
2018-09-24 23:25:45 +0800

    Digital Root

---
+++ DigitalRoot/DigitalRoot/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace DigitalRoot
+{
+    public class Kata
+    {
+        public int DigitalRoot(int input)
+        {
+            input = input.ToString().ToCharArray().Aggregate(0, (current, i) => (int) i - 48 + current);
+            return input > 10 ? DigitalRoot(input) : input;
+        }
+    }
+}
---
+++ DigitalRoot/DigitalRootTests/KataTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DigitalRoot.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void One_Six_Should_Return_Seven(int expect, int input)
+        {
+            Assert.AreEqual(7, new Kata().DigitalRoot(16));
+        }
+
+        [TestMethod()]
+        public void Four_Five_Six_Should_Return_Six()
+        {
+            Assert.AreEqual(6, new Kata().DigitalRoot(456));
+        }
+
+        [TestMethod()]
+        public void Nine_Nine_Nine_Nine_Should_Return_Nine()
+        {
+            Assert.AreEqual(9, new Kata().DigitalRoot(9999));
+        }
+    }
+}
